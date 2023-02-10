70c2c235f842817808a4b8a744a2be976da13fbb
ydeng <dengyujun11@gmail.com>
2023-02-10 00:10:40 -0500

    create stubFactory to provide BlockingStub

---
+++ src/main/java/today/ihelio/paxos/StubFactoryProvider.java
@@ -0,0 +1,15 @@
+package today.ihelio.paxos;
+
+import javax.inject.Provider;
+import today.ihelio.paxos.utility.StubFactory;
+import today.ihelio.paxoscomponents.PaxosServerServiceGrpc;
+
+public class StubFactoryProvider implements
+    Provider<StubFactory> {
+
+
+
+  @Override public StubFactory get() {
+    return null;
+  }
+}
---
+++ src/main/java/today/ihelio/paxos/utility/StubFactory.java
@@ -0,0 +1,4 @@
+package today.ihelio.paxos.utility;
+
+public class StubFactory {
+}

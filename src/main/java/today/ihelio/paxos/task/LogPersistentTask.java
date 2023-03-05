b1f4f2ffd7e02e9d825ce03fecf9c7eb42ca46ca
ydeng <dengyujun11@gmail.com>
2023-03-05 17:54:29 -0500

    write the hash value to local file

--- src/main/java/today/ihelio/paxos/task/LogPersistentTask.java
+++ src/main/java/today/ihelio/paxos/task/LogPersistentTask.java
@@ -1,40 +1,48 @@
 package today.ihelio.paxos.task;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import today.ihelio.paxos.PaxosServer;
 
 @Singleton
 public class LogPersistentTask implements Runnable {
+  private final Logger logger = LoggerFactory.getLogger(LogPersistentTask.class);
   private final PaxosServer paxosServer;
-  private long lastSeen;
-
+  private final Path FILEPATH;
   @Inject
-  public LogPersistentTask(PaxosServer paxosServer) {
+  public LogPersistentTask(PaxosServer paxosServer) throws IOException {
     this.paxosServer = paxosServer;
-    lastSeen = System.currentTimeMillis();
+    this.FILEPATH = Paths.get("/Users/ydeng/code/Multi-Paxos/FakePersistentStore/"
+        + paxosServer.getHostID() + ".txt");
+    if (!Files.exists(FILEPATH)) {
+      Files.createFile(FILEPATH);
+    }
   }
 
   @Override public void run() {
     while (true) {
-      if (System.currentTimeMillis() - lastSeen > 5000) {
-        try (FileWriter fw = new FileWriter("/Users/ydeng/code/Multi-Paxos/FakePersistentStore/"
-            + paxosServer.getHostID() + ".txt", true)) {
-          BufferedWriter bw = new BufferedWriter(fw);
-          PrintWriter out = new PrintWriter(bw);
-          out.println(paxosServer);
-          out.println();
-        } catch (FileNotFoundException e) {
-          throw new RuntimeException(e);
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-        lastSeen = System.currentTimeMillis();
+      try {
+        Thread.sleep(5000);
+        Files.writeString(FILEPATH, String.valueOf(System.currentTimeMillis())
+            + " - Hash: " + paxosServer + "\n", StandardOpenOption.APPEND);
+        logger.debug("valueArray hash: " + paxosServer + "\n");
+        logger.debug("firstUnchosen: " + paxosServer.getFirstUnchosenIndex());
+        logger.debug("hash: " + paxosServer);
+      }
+      catch (InterruptedException e) {
+        logger.info("InterruptedException: " + e.getMessage());
+        throw new RuntimeException(e);
+      }
+      catch (IOException e) {
+        logger.info("Got IOException: " + e.getMessage());
+        throw new RuntimeException(e);
       }
     }
   }
